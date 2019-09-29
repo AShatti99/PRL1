dc724ace7df314578a7baa741ac3de94cc526fbd
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2019-09-29 12:30:35 +0200

    Create es1.c

---
+++ Lab/Lezione 2/es1.c
@@ -0,0 +1,15 @@
+/*
+Aprite l’editor gedit
+Create un file hello.c
+Scrivete in hello.c il vostro personale Hello World!
+Aprite una shell e posizionatevi sulla directory dove avete
+salvato hello.c
+Compilate hello.c usando GCC ed eseguitelo 
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    printf("Hello World!\n");
+}

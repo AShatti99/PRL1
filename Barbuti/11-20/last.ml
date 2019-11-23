b9ffd7ad7d75c8749d91b6e1970dee157c3017eb
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2019-11-23 10:23:57 +0100

    Create last.ml

---
+++ Barbuti/11-20/last.ml
@@ -0,0 +1,30 @@
+(* Data una lista vogliamo scrivere una funzione caml che restituisce l'ultimo elemento di una lista.
+La funzione e' indefinita su lista vuota 
+
+last[3; 4; 5] = 5 *)
+
+#let rec last l =
+    if tl l= [] then hd last
+    else last(tl l);;
+
+#last [3; 4; 5];;
+(* -: int =5 *)
+
+
+(*
+
+last [3; 4; 5];;
+={definizione di last, l=[3; 4; 5] }
+
+last [4; 5];;
+={definizione di last, l=[4;5] } 
+
+last [5];;
+={definizione di last, l=[5] }
+
+hd[5];;
+={definizione di hd}
+
+5
+
+*)

d0390eea85bd83b43b6b22b0600d3d57b0e94b0c
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2019-11-29 23:41:57 +0100

    Create max.ml

---
+++ Barbuti/11-25/max.ml
@@ -0,0 +1,10 @@
+(* Cercare il valore massimo di una lista. Funzione non definita su [] *)
+
+#let rec max l =
+    match l with
+    [x] -> x
+    | x :: y :: ys ->
+        if x > max (y :: ys) then xs 
+        else max (y :: ys);;
+
+(* max : 'a list -> 'a = <fun> *)

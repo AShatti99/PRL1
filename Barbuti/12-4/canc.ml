4f84a1d9898c25a085f6da4b9a566a722028b66c
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2019-12-09 10:40:34 +0100

    Update canc.ml

--- Barbuti/12-4/canc.ml
+++ Barbuti/12-4/canc.ml
@@ -16,3 +16,25 @@ canc 3 [1; 2] = []
     | x :: xs -> if len(x :: xs) <= n then []
                  else x :: canc n xs;;
 (* canc: int -> 'a list -> 'a list *)
+
+
+(* SECONDA SOLUZIONE*)
+
+(* qui cancelli i primi n elementi *)
+#let rec drop n l=
+    match (n,l) with    
+    (0,l) -> l
+    | (n, []) when n>0 -> []
+    | (n, x :: xs) when n>0 -> drop(n-1) xs;;
+
+(* qui rovesci la lista *)
+#let rec rev l=
+        match l with
+        [] -> []
+        | x :: xs -> rev(xs) @ [x];;
+
+(* qui unisce le due funzioni *)
+#let canc n l=
+    rev(drop n (rev l));;
+
+(* Rovesci la lista -> cancelli i primi n elementi -> rovesci un'altra volta la lista *)

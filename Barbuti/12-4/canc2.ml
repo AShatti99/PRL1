5588a7799ee6e494f523e3793cfe3fcb3a6d927a
AShatti99 <52958874+AShatti99@users.noreply.github.com>
2019-12-09 11:17:47 +0100

    Update canc2.ml

--- Barbuti/12-4/canc2.ml
+++ Barbuti/12-4/canc2.ml
@@ -1,5 +1,20 @@
 (* Funzione che cancella gli elementi in fondo a una lista e mi dice quanti ne ha cancellate *)
 
+#let canc n l=
+    let rec canc_n n l=
+            match l with
+            [] -> ([],0)
+            | x :: xs -> let (l1,m) = canc_n n xs
+                         in 
+                         if m<n then (l1, m+1)
+                         else (x :: l1, m);
+    
+    in let(l1,m) = canc_n n l
+       in l1;;
+
+
+(* Con foldr *)
+
 #let rec foldr f a l=
     match l with
     [] -> a
